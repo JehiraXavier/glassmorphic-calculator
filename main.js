d9c66072d3d116b946086124ba980f2902944732
JehiraXavier <jehiraxavier@gmail.com>
2023-06-22 15:28:21 +0530

    Code cleanup

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
 //Function To Display Values On screen
 function display(value) {
-    document.getElementById("screen").value += value;
+  document.getElementById("screen").value += value;
 }
 
 // Function For Evaluation
 function solve() {
-    let x = document.getElementById("screen").value;
-    let y = eval(x);
-    document.getElementById("screen").value = y;
+  let x = document.getElementById("screen").value;
+  let y = eval(x);
+  document.getElementById("screen").value = y;
 }
 
 //Function For clearing the screen
 function clear() {
-    document.getElementById("screen").value = ""
+  document.getElementById("screen").value = "";
 }
 
-//backspacing
+//Backspacing
 function backspace() {
-    let screen = document.getElementById("screen").value;
-    document.getElementById("screen").value = screen.substring(0, screen.length - 1);
+  let screen = document.getElementById("screen").value;
+  document.getElementById("screen").value = screen.substring(
+    0,
+    screen.length - 1
+  );
 }
